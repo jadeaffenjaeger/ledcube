ab4129362e05590199760905566eb1abed721ae2
e.trommer <e.trommer@campus.tu-berlin.de>
2018-06-15 23:24:20 +0200

    Untested Pixel extrapolation

--- firmware/cube/Inc/cube.h
+++ firmware/cube/Inc/cube.h
@@ -9,10 +9,18 @@
 #define SIZE 8
 #define RESOLUTION 4
 
+typedef struct SExtrapolation {
+    uint8_t p1;
+    uint8_t p2;
+} SExtrapolation_t;
+
 void cube_init(void);
 void cube_fillBitBuffer(uint8_t bitPosition);
 uint8_t* cube_getNext(void);
 void cube_update(void);
+
+void cube_drawPoint(uint8_t x, uint8_t y, uint8_t z, uint8_t value);
+void cube_setPx(uint8_t x, uint8_t y, uint8_t z, uint8_t value);
 
 #ifdef __cplusplus
 }
