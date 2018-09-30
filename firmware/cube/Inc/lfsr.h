ce4f70b69578bbdbf517c66ff7a7978b86f58216
Elias <elias_trommer@gmx.de>
2018-09-30 13:35:42 +0200

    Add comments to LFSR implementation

--- firmware/cube/Inc/lfsr.h
+++ firmware/cube/Inc/lfsr.h
@@ -6,12 +6,22 @@ extern "C" {
 
 #include "stm32f1xx.h"
 
+/*
+ *Defines polynomial and current state of the LFSR
+ */
 typedef struct lfsr {
     uint32_t mask;
     uint32_t state;
 } lfsr_t;
 
+/*
+ *Get pseudo-random 8 Bit sequence
+ */
 uint8_t lfsr_get8(lfsr_t* reg);
+
+/*
+ *Get pseudo-random 32 Bit sequence
+ */
 uint32_t lfsr_get32(lfsr_t* reg);
 
 #ifdef __cplusplus
